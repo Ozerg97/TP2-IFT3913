c931e935b5f4890c301f97c3d731e24f7dd615be
Keepa10 <paul.konan@umontreal.ca>
2023-10-26 22:24:42 -0400

    Update NCH.java

--- Q2_branch/TP2_IFT3913/src/main/java/NCH.java
+++ Q2_branch/TP2_IFT3913/src/main/java/NCH.java
@@ -6,12 +6,12 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-public class NCH{
+public class NCH {
 
     public static int compteur(String repositoryPath, String className) throws IOException, GitAPIException {
         try (Git git = Git.open(Paths.get(repositoryPath).toFile());
-             RevWalk revWalk = new RevWalk(git.getRepository())){
-        
+                RevWalk revWalk = new RevWalk(git.getRepository())) {
+
             Iterable<RevCommit> commits = git.log().all().call();
             int entier = 0;
             for (RevCommit commit : commits) {
@@ -26,16 +26,14 @@ public class NCH{
     }
 
     public static void main(String[] args) {
-        String repositoryPath = "/Users/paulkonan/Documents/GitHub/tp1_3913";
-    String className = "tropComp jar";
+        String repositoryPath = "/Users/user1/Documents/GitHub/tp1_3913"; // mettre le chemin ici
+        String className = "tropCom.java"; // mettre le nom de la classe ici
 
-    try
-    {
+        try {
             int commitCount = compteur(repositoryPath, className);
             System.out.println("Number of commits: " + commitCount);
-        }catch(IOException|
-    GitAPIException e)
-    {
-        e.printStackTrace();
+        } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
+        }
     }
-}}
+}
